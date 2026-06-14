daabc46b3c173ae24f4ce1aee29169164638682f
Nurollah <norollah10134949@gmail.com>
2026-06-14 11:36:58 +0200

    fixing BookingForm.jsx

--- barbershop/src/BookingForm.jsx
+++ barbershop/src/BookingForm.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useLang } from "./LanguageContext";
 import LangSwitcher from "./LangSwitcher";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export default function BookingForm() {
   const { t, lang } = useLang();
